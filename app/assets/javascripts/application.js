dbacd00b8baf8649ed0f818339751d732a7df453
Kelly Millward <kellymillward@SLWT628697.AD.HQ.DEPT>
2023-06-29 12:24:03 +0100

    updates based on team feedback

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,10 +22,42 @@ $(window).resize(function() {
 }).resize(); 
 
 
-$('.govuk-header__container').append("<div class='header-links'><p>Joe Bloggs</p><ul><li><a href='#' class='header-sign-out'>Sign out</a></li></ul>");
+// $('.govuk-header__container').append("<div class='header-links'><p>Joe Bloggs</p><ul><li><a href='#' class='header-sign-out'>Sign out</a></li></ul>");
 
 
-$('.govuk-cookie-banner').insertBefore('.govuk-header');
-$('#app-navigation').insertAfter('.govuk-header');
-$('.govuk-phase-banner').insertAfter('#app-navigation');
+// $('.govuk-cookie-banner').insertBefore('.govuk-header');
+// $('#app-navigation').insertAfter('.govuk-header');
+// $('.govuk-phase-banner').insertAfter('#app-navigation');
 $(".app-section").insertAfter(".govuk-phase-banner");
+
+//add active classes to urls
+if (window.location.href.indexOf("index") != -1){ 
+  $('#app-navigation li:nth-child(1)').addClass('active');
+}
+if (window.location.href.indexOf("search") != -1){ 
+  $('#app-navigation li:nth-child(2)').addClass('active');
+}
+if (window.location.href.indexOf("my-school") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
+if (window.location.href.indexOf("key-stage-1") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
+if (window.location.href.indexOf("key-stage-2") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
+if (window.location.href.indexOf("key-stage-4") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
+if (window.location.href.indexOf("phonics") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
+if (window.location.href.indexOf("mtc") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
+if (window.location.href.indexOf("qla") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
+if (window.location.href.indexOf("other-reports") != -1){ 
+  $('#app-navigation li:nth-child(3)').addClass('active');
+}
